d9e11320e50020e53462be551f4dea33eb97bf20
huqingfeng <1397649957@qq.com>
2021-08-26 17:08:02 +0800

    port

--- src/main/java/io/github/jzdayz/controller/ExController.java
+++ src/main/java/io/github/jzdayz/controller/ExController.java
@@ -2,6 +2,7 @@ package io.github.jzdayz.controller;
 
 import io.github.jzdayz.ex.TableNotFoundException;
 import io.github.jzdayz.rs.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,15 +13,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@Slf4j
 public class ExController {
 
     @ExceptionHandler(TableNotFoundException.class)
-    public R<?> tableNotFoundException(){
+    public R<?> tableNotFoundException() {
         return R.error("表不存在！");
     }
 
     @ExceptionHandler(Exception.class)
-    public R<?> ex(Exception e){
+    public R<?> ex(Exception e) {
+        log.error(e.getMessage(), e);
         return R.error(e.getMessage());
     }
 
@@ -28,9 +31,9 @@ public class ExController {
     @ResponseBody
     public R<?> bindException(BindException ex) {
         R<Object> error = R.error("");
-        Map<String,String> data = new HashMap<>();
+        Map<String, String> data = new HashMap<>();
         for (FieldError fieldError : ex.getFieldErrors()) {
-            data.put(fieldError.getField(),fieldError.getDefaultMessage());
+            data.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
         error.setData(data);
         return error;
